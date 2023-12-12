6733967d44e12eea9f9ded8ea6c3210441010885
Nomad-Rob <rfarley89@gmail.com>
2023-12-11 19:06:37 -0600

    saving what I have for not. Still working on errors. Changed css to scss. Got button working

--- rob-src/js/index.js
+++ rob-src/js/index.js
@@ -1,12 +1,30 @@
 import * as THREE from 'three'
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
-import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader'
-import {Text} from 'troika-three-text'
-import {gsap, Power1} from 'gsap'
+import { gsap, Power1 } from 'gsap'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+
+
+const counterLoading = document.querySelector(".counterLoading")
+const header = document.querySelector("header")
+const h1 = document.querySelector("h1")
+const footer = document.querySelector("footer")
+const loading = document.querySelector(".loading")
+const started = document.querySelector(".started")
+const startedBtn = document.querySelector(".started-btn")
+
+let videoLook = false
+let scrollI = 0.0
+let initialPositionMeshY = -1
+let initialRotationMeshY = Math.PI * 0.9
+
+let isLoading = false
 
 
 // Debug
 const debugObject = {}
+
+// Canvas
+const canvas = document.querySelector(".main-webgl")
 
 // Scene
 const scene = new THREE.Scene();
@@ -16,9 +34,9 @@ scene.background = new THREE.Color("#ffff");
 const backgroundScene = new THREE.Scene()
 
 // Sizes
-const sizes = {
+const sizesCanvas = {
   width: window.innerWidth,
-  height: window.innerHeight,
+  height: window.innerHeight
 }
 
 // Event Listener
@@ -48,9 +66,10 @@ window.addEventListener('mousemove', (event) => {
 })
 
 // Audio
-const music = new Audio("/sounds/christmas.mp3")
+const music = new Audio("sounds/christmas.mp3")
 music.volume = 0.3
-``
+
+
 // Loaders
 const loadingManager = new THREE.LoadingManager(
   () => {
@@ -79,13 +98,13 @@ const loadingManager = new THREE.LoadingManager(
         ease: Power1.easeIn,
       })
       // Loading Percentage Counter
-      gsap.to(loadingLoading, 0.5, {
+      gsap.to(counterLoading, 0.5, {
         delay: 0.5,
         opacity: 0,
         ease: Power1.easeIn,
       })
-      // Starter
-      gsap.to(starter, 0.5, {
+      // Started
+      gsap.to(started, 0.5, {
         delay: 0.9,
         opacity: 1,
       })
@@ -121,8 +140,8 @@ const loadingManager = new THREE.LoadingManager(
 const continueAnimation = () => {
   // Music and sounds here
   music.play()
-  // Starter
-  gsap.to(starter, 0.5, {
+  // Started
+  gsap.to(started, 0.5, {
     opacity: 0
   })
   // Loading
@@ -138,10 +157,10 @@ const continueAnimation = () => {
   })
   //// Setting a timeout to execute the following block of code after a delay of 250 milliseconds.
   setTimeout(() => {
-    // Changing the visibility of the 'loading and starter' element to 'hidden'.
+    // Changing the visibility of the 'loading and started' element to 'hidden'.
     // Also setting the visibility of the 'groupPlane' and 'groupText' elements to 'visible'.
     loading.style.visibility = "hidden"
-    starter.style.visibility = "hidden"
+    started.style.visibility = "hidden"
     groupPlane.visible = true
     groupText.visible = true
     // Setting the 'isLoading' variable to true.
@@ -153,10 +172,10 @@ const continueAnimation = () => {
 // Addiing images to the scene
 const textureLoader = new THREE.TextureLoader(loadingManager)
 
-const imagesLoad1 = textureLoader.load("/images/cozy_room.jpg")
-const imagesLoad2 = textureLoader.load("/images/light_made_tree.jpg")
-const imagesLoad3 = textureLoader.load("/images/lightpost.jpg")
-const imagesLoad4 = textureLoader.load("/images/sleeping_dog.jpg")
+const imagesLoad1 = textureLoader.load("./images/cozy_room.jpg")
+const imagesLoad2 = textureLoader.load("./images/light_made_tree.jpg")
+const imagesLoad3 = textureLoader.load("./images/lightpost.jpg")
+const imagesLoad4 = textureLoader.load("./images/sleeping_dog.jpg")
 
 // Create a new GLTFLoader instance, passing the loadingManager to handle loading events.
 const gltfLoader = new GLTFLoader(loadingManager)
@@ -166,7 +185,7 @@ let models = []
 // Loading Santa Model
 // Initialize the GLTFLoader with the loadingManager.
 gltfLoader.load(
-  "/models/santa/scene.gltf",
+  "models/santa/scene.gltf",
   (gltf) => { // Callback function that is executed once the model has loaded.
     // Set the scale of the model. This uniformly scales the model in all three dimensions.
     gltf.scene.scale.set(5, 5, 5)
@@ -199,7 +218,7 @@ let startTouch = 0
 
 // Load Rock model
 gltfLoader.load(
-    "models/rock.glb", // Path to the .glb file
+    "models/rock/scene.gltf", // Path to the .glb file
     (gltf) => {
         // Scale, position, and rotation settings remain the same as before
         gltf.scene.scale.set(2.5, 2, 2.5)
@@ -262,7 +281,7 @@ camera.position.z = - 5
 scene.add(camera)
 
 // Background camera with orthographic camera
-const backgroundCamera = new THREE.orthographicCamera(-1, 1, 1, -1, -1, 0)
+const backgroundCamera = new THREE.OrthographicCamera(-1, 1, 1, -1, -1, 0)
 
 // Controls
 const controls = new OrbitControls(camera, canvas)
@@ -279,3 +298,121 @@ pointLight.position.set (-5.5, 5.5, -5)
 scene.add(pointLight)
 
 
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+  alpha: true,
+})
+
+renderer.setSize(sizesCanvas.width, sizesCanvas.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.autoClear = false
+
+
+// Animation
+const animationScroll = (eventObject, touchEvent, valuse, downOrUp) => {
+  let deltaY
+  
+  if (touchEvent) deltaY = value
+  else deltaY = eventObject.deltaY
+  
+  if (videoLook === false && isLoading) {
+    // Known up or down
+    if (toucheEvent && downOrUp === "down" && scrollI > 0) scrollI--
+    else if (!toucheEvent && deltaY < 0 && scrollI > 0) scrollI00
+    
+    if (scrollI <= 435 && scrollI >= 0 && models.length === 2) {
+      if (touchEvent && downOrUp === "up") scrollI++
+      else if (!touchEvent && deltaY > 0) scrollI++
+      const speed = 0.005
+      
+      
+      // Update Mesh
+      
+      models.forEach((model, index) => {
+        // rotation
+        model.rotation.y = (initialRotationMeshY) - scrollI * 0.01355 // End front of camera
+    
+        // position
+        if (index === 0) model.position.y = (initialPositionMeshY) - scrollI * (speed * 0.8)
+        else if (index === 1) model.position.y = (initialPositionMeshY - 1.73) - scrollI * (speed * 0.8)
+
+        model.position.z = - scrollI * (speed * 0.75)
+    })
+    
+    // Update group of planes
+    
+    for (let i = 0; i < groupPlane.children.length; i++) {
+      const plane = groupPlane.children[i]
+      const text = groupText.children[i]
+
+      // Planes -------
+      // Position
+      plane.position.z = - Math.sin(i + 1 * scrollI * (speed * 10)) * Math.PI
+      plane.position.x = - Math.cos(i + 1 * scrollI * (speed * 10)) * Math.PI
+      plane.position.y = (i - 14.2) + (scrollI * (speed * 10))
+
+      // Rotation
+      plane.lookAt(0, plane.position.y, 0)
+
+      // Text -------
+      // Position
+      text.position.z = plane.position.z - 0.5
+      text.position.x = plane.position.x
+      text.position.y = plane.position.y - 0.3
+
+      // Rotation
+      text.lookAt(plane.position.x * 2, plane.position.y - 0.3, plane.position.z * 2)
+  }
+}
+}
+}
+
+const clock = new THREE.Clock()
+
+let callChangeTouchValue = 0
+let touchI = - 1
+
+const init = () => {
+    const elapsedTime = clock.getElapsedTime()
+        
+    // Upadate raycaster
+    if(!("ontouchstart" in window)) raycatser.setFromCamera(mouse, camera)
+
+    // black and white to colo animation with raycaster
+    if (isLoading) {
+        if (intersects.length === 1) {
+            if (currentIntersect === null) {
+                currentIntersect = intersects[0]
+            } else {
+                for (let i = 0; i < groupPlane.children.length; i++) {
+                    if (groupPlane.children[i] === currentIntersect.object) {
+                        if (callChangeTouchValue === 0) {
+                            touchI = i
+                            changeTouchValue(i)
+                            callChangeTouchValue = 1
+                            document.body.style.cursor = "pointer"               
+                        }
+                    }
+                }
+            }
+        } else {
+            if (callChangeTouchValue === 1 && touchI >= 0) {
+                changeTouchValue(touchI)
+                callChangeTouchValue = 0
+                document.body.style.cursor = "auto" 
+                currentIntersect = null
+                touchI = - 1
+            }
+        }
+    }
+
+    // Update renderer
+    renderer.render(scene, camera)
+    renderer.render(backgroundScene, backgroundCamera)
+
+    // Call this function
+    window.requestAnimationFrame(init)
+}
+
+init()
