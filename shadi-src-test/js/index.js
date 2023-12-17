5a47fb768aad497c1711950fa70ff98754b6ff37
Shadi-Shwiyat <raz611103@gmail.com>
2023-12-17 13:43:57 -0600

    halfway done with meshing code, having issues with switching cameras

--- shadi-src-test/js/index.js
+++ shadi-src-test/js/index.js
@@ -1,6 +1,8 @@
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
 import gsap from 'gsap';
+import { Power1 } from 'gsap';
 import * as dat from 'dat.gui';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 // Sizes
 const sizes = {
@@ -18,38 +20,29 @@ const loading = document.querySelector('.loading');
 const started = document.querySelector('.started');
 const startBtn = document.querySelector('.start-btn');
 
-// Scene variables
-let touchValue = 1;
-let videoLook = false;
-let scroll = 0.0;
-let initialPositionMeshY = -1;
-let initialRotationMeshY = Math.PI * 0.9;
-let planeClickedIndex = -1;
-let isLoading = false;
-let lastPosition = {
-    px: null,
-    py: null,
-    pz: null,
-    rx: null,
-    ry: null,
-    rz: null
-};
-let cameraZPosition = 18;
-let santaRotation = 0;
-let sleighRotation = 0;
-let imageIndex = 0;
-let revealSpeed = 0.1; // Adjust the speed of image reveal
+// Debug object for future enhancements
+const debugObject = {};
 
 // dat.Gui instantiation
 const gui = new dat.GUI();
 
-// Camera
-const camera = new THREE.PerspectiveCamera(60, sizes.width / sizes.height, 0.1, 600);
-camera.position.z = 160;
-// camera.lookAt(0, 83, 0);
+// Timeline instantiation
+const tl = gsap.timeline({defaults: {duration: 1}});
+
+// Main page Camera
+const mainCamera = new THREE.PerspectiveCamera(60, sizes.width / sizes.height, 0.1, 600);
+mainCamera.position.z = 160;
+// mainCamera.lookAt(0, 83, 0);
 
 // Import canvas
 const canvas = document.querySelector(".webgl");
+
+// Rob's Santa Scene
+const santaScene = new THREE.Scene();
+santaScene.background = new THREE.Color('#333333');
+
+// Main Snow Scene
+const snowScene = new THREE.Scene();
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({canvas});
@@ -57,13 +50,46 @@ renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(3);
 
 // Controls, set initally to false when page is loaded
-const controls = new OrbitControls(camera, canvas);
-controls.enabled = false;
+const controls = new OrbitControls(mainCamera, canvas);
+setupControls();
 
+// Mouse move tracking
+let mouse = new THREE.Vector2();
+window.addEventListener('mousemove', onMouseMove);
+
+// Audio setup
+const music = new Audio('sounds/christmas.mp3');
+music.volume = 0.00;
+
+// Loaders setup
+const loadingManager = new THREE.LoadingManager(onLoadComplete, onProgress);
+const textureLoader = new THREE.TextureLoader(loadingManager);
+const gltfLoader = new GLTFLoader(loadingManager);
+
+// Array to store loaded models
+let models = [];
+
+// Load models (Santa and Sleigh)
+loadModels();
+
+// Santa Camera setup
+const santaCamera = setupSantaCamera();
+santaScene.add(santaCamera);
+
+// Background camera setup (Orthographic)
+const backgroundCamera = new THREE.OrthographicCamera(-1, 1, 1, -1, -1, 0);
+
+// Choose correct camera to display
+switchCamera('mainCamera');
+
+// Lighting setup
+setupLighting();
+
+// Event listener for player close
+playerClose.addEventListener('click', onClosePlayer);
 
 // *********************************************************
 // Snowflakes
-const snowScene = new THREE.Scene();
 let particles;
 let positions = [], velocities = []; // snowflake positions(x, y, z) and velocities(x, y, z) 
 
@@ -198,9 +224,365 @@ leftMountainFgMesh.position.set(-98, -53.9, -6);
 snowScene.add(bgMesh, santaMesh, leftMountainBgMesh, rightMountainBgMesh, mainMountainMesh, rightMountainPreBgMesh, middleMountainMesh, rightMountainPreFgMesh, rightRockFgMesh, leftMountainFgMesh);
 // *********************************************************
 
+// *********************************************************
+// Santa page functions (integrating Rob's code)
+
+function onMouseMove(event) {
+  // Update mouse position
+  mouse.x = event.clientX / sizes.width * 2 - 1;
+  mouse.y = -(event.clientY / sizes.height) * 2 + 1;
+}
+
+function onLoadComplete() {
+  // Hide the loading percentage counter and display the main elements
+  gsap.to(counterLoading, {
+      opacity: 0,
+      duration: 0.5,
+      onComplete: () => {
+          counterLoading.style.display = 'none';
+      }
+  });
+
+  // Animate footer
+  gsap.to(footer, {
+      delay: 0.5,
+      opacity: 1,
+      ease: Power1.easeIn,
+      duration: 0.5
+  });
+
+  // Reveal the 'started' button with an animation
+  gsap.to(started, {
+      delay: 0.9,
+      opacity: 1,
+      duration: 0.5
+  });
+
+  // Add an event listener to the 'startBtn' element
+  startBtn.addEventListener('click', () => {
+      continueAnimation();
+      console.log('startBtn clicked');
+      
+      fetch('images.json')
+      .then(response => response.json())
+      .then(data => {
+        displayImagesAndText(data);
+      })
+  });
+
+  function displayImagesAndText(data) {
+    // Create a group to hold the images and text
+    const imageGroup = new THREE.Group();
+    santaScene.add(imageGroup);
+
+    const numImages = data.length;
+    const radius = 50; // Adjust the radius for spacing between images
+    const angleIncrement = (2 * Math.PI) / numImages;
+
+    // Get the position of the santa.glb model
+    const santaPosition = models[0].position;
+
+    data.forEach((item, index) => {
+      // Calculate the position of the image based on polar coordinates
+      const angle = angleIncrement * index;
+      const imageX = santaPosition.x + radius * Math.cos(angle);
+      const imageY = santaPosition.y * (index + 1); // Adjust the Y position
+      const imageZ = santaPosition.z + radius * Math.sin(angle);
+
+      // Load image texture
+      const texture = textureLoader.load(item.imageUrl);
+
+      // Create a plane with the image texture
+      const imagePlane = new THREE.Mesh(
+          new THREE.PlaneGeometry(10, 10), // Adjust the size as needed
+          new THREE.MeshBasicMaterial({ map: texture, side: THREE.DoubleSide })
+      );
+
+      // Set the position and rotation of the image
+      imagePlane.position.set(imageX, imageY, imageZ);
+      imagePlane.lookAt(santaPosition);
+
+      // Create a text sprite
+      const textSprite = createTextSprite(item.text, item.year);
+
+      // Set the position of the text relative to the image
+      const textOffsetX = 5; // Adjust this value as needed
+      const textOffsetZ = 5;
+      textSprite.position.set(imageX + textOffsetX, imageY, imageZ + textOffsetZ);
+
+      // Set the rotation of the text to match the image
+      textSprite.lookAt(santaPosition);
+
+      // Add the image and text to the group
+      imageGroup.add(imagePlane);
+      imageGroup.add(textSprite);
+    });
+  }
+}
+
+function createTextSprite(text, year) {
+  // Create a canvas element to generate the text sprite
+  const canvas = document.createElement('canvas');
+  const context = canvas.getContext('2d');
+  canvas.width = 800;
+  canvas.height = 600;
+
+  // Customize the appearance of the text
+  context.font = '60px Arial'; // Adjust font size and style as needed
+  context.fillStyle = 'white'; // Adjust text color as needed
+  context.textAlign = 'center';
+
+  // Split the text into words
+  const words = text.split(' ');
+
+  // Calculate the maximum number of words that can fit in a line
+  const maxWordsPerLine = 5; // Adjust as needed
+
+  // Calculate the maximum number of lines that can fit in the canvas
+  const maxLines = 2; // Adjust as needed
+
+  // Calculate the maximum number of words that can fit in the text sprite
+  const maxWords = maxWordsPerLine * maxLines;
+
+  // Create an array to store the lines of text
+  const lines = [];
+
+  // Iterate through the words and create lines of text
+  let currentLine = '';
+  for (let i = 0; i < words.length; i++) {
+    const word = words[i];
+    const testLine = currentLine + word + ' ';
+    const metrics = context.measureText(testLine);
+    const lineWidth = metrics.width;
+
+    if (lineWidth > canvas.width && i > 0) {
+      lines.push(currentLine);
+      currentLine = word + ' ';
+    } else {
+      currentLine = testLine;
+    }
+
+    if (lines.length >= maxLines) {
+      break;
+    }
+  }
+
+  // Push the remaining words as the last line
+  lines.push(currentLine);
+
+  // Draw the lines of text on the canvas
+  const lineHeight = 60; // Adjust line height as needed
+  const startY = (canvas.height - (lines.length * lineHeight)) / 2;
+  for (let i = 0; i < lines.length; i++) {
+    const line = lines[i];
+    const y = startY + (i * lineHeight);
+    context.fillText(line, canvas.width / 2, y);
+  }
+
+  // Draw the year underneath the text
+  context.font = '40px Arial'; // Adjust font size and style as needed
+  const yearLineHeight = 40; // Adjust line height for the year
+  const yearY = startY + (lines.length * lineHeight) + yearLineHeight; // Adjust the vertical position of the year
+  context.fillText(year, canvas.width / 2, yearY);
+
+  // Create a texture from the canvas
+  const texture = new THREE.CanvasTexture(canvas);
+
+  // Create a sprite with the texture
+  const spriteMaterial = new THREE.MeshBasicMaterial({ map: texture, side: THREE.DoubleSide });
+  const spriteGeometry = new THREE.PlaneGeometry(10, 10);
+  const spriteMesh = new THREE.Mesh(spriteGeometry, spriteMaterial);
+
+  // Scale the sprite to match the canvas size
+  spriteMesh.rotateX(180);
+
+  return spriteMesh;
+}
+
+// Setting a timeout to ensure that certain actions only happen after the rest of the script has loaded
+setTimeout(() => {
+}, 50);
+
+function onProgress(itemUrl, itemsLoaded, itemsTotal) {
+  // Update progress for loading resources
+  const progressRatio = itemsLoaded / itemsTotal;
+  counterLoading.innerHTML = `${(progressRatio * 100).toFixed(0)}%`;
+}
+
+function loadModels() {
+  // Load Santa Model
+  gltfLoader.load(
+      "models/santa.glb",
+      (gltf) => {
+          gltf.scene.scale.set(5, 5, 5);
+          // gltf.santaScene.position.y = initialPositionMeshY 0;
+          gltf.scene.position.z = 0;
+          gltf.scene.position.y = -5;
+          gltf.scene.position.x = 0;
+          gltf.scene.rotation.x = 0;
+          // gltf.santaScene.rotation.y = initialRotationMeshY 0
+
+          santaScene.add(gltf.scene);
+          models.push(gltf.scene);
+
+          santaScene.traverse((child) => {
+              if (child instanceof THREE.Mesh && child.material instanceof THREE.MeshStandardMaterial) {
+                  child.material.envMapIntensity = debugObject.envMapIntensity;
+                  child.material.needsUpdate = true;
+              }
+          });
+      },
+      undefined,
+      (error) => {
+          console.error('An error occurred while loading the Santa model:', error);
+      }
+  );
+
+  // Load Sleigh Model
+  gltfLoader.load(
+      "models/sleigh.glb",
+      (gltf) => {
+          gltf.scene.scale.set(0.05, 0.05, 0.05);
+          gltf.scene.position.y = -7.8;
+          // gltf.santaScene.position.y = initialPositionMeshY 0;
+          gltf.scene.position.x = 0;
+          gltf.scene.position.z = -1.8;
+          gltf.scene.rotation.y = -1.6;
+          
+          // gltf.santaScene.rotation.y = initialRotationMeshY 0;
+
+          santaScene.add(gltf.scene);
+          models.push(gltf.scene);
+      },
+      undefined,
+      (error) => {
+          console.error('An error occurred while loading the Sleigh model:', error);
+      }
+  );
+}
+
+function setupSantaCamera() {
+  // Set up and return the main santaCamera
+  const santaCamera = new THREE.PerspectiveCamera(80, sizes.width / sizes.height, 0.1, 250);
+  santaCamera.name = 'Santa Camera';
+  santaCamera.position.x = 0;
+  santaCamera.position.y = 5;
+  santaCamera.position.z = 30;
+  return santaCamera;
+}
+
+function setupControls() {
+  // Set up orbit controls
+  controls.enabled = true;
+  controls.enableZoom = true;
+}
+
+function setupLighting() {
+  // Set up santaScene lighting (ambient and point lights)
+  const ambientLight = new THREE.AmbientLight(0xffffff, 1.5);
+  santaScene.add(ambientLight);
+
+  const pointLight = new THREE.PointLight(0xffffff, 15);
+  pointLight.position.set(0,0,0);
+  santaScene.add(pointLight);
+}
+
+function setupRenderer() {
+  // Configure the WebGL renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+  renderer.autoClear = false;
+}
+
+function continueAnimation() {
+  // Music and sounds here, continuous playing
+  music.loop = true;
+  music.play();
+  
+
+  // Animate the opacity of 'started' to 0
+  gsap.to(started, {
+      opacity: 0,
+      duration: 0.5
+  });
+
+  // Animate the opacity of 'loading' to 0
+  gsap.to(loading, {
+      opacity: 0,
+      duration: 0.5
+  });
+
+  // Animate the santaCamera position after
+  gsap.to(santaCamera.position, {
+      delay: 1,
+      x: 0,
+      y: 5,
+      z: 18,
+      duration: 1
+  });
+  
+  setTimeout(() => {
+    loading.style.visibility = "hidden"
+    started.style.visibility = "hidden"
+  }, 250);
+  
+
+  // Set a timeout to execute the following block of code after a delay of 250 milliseconds
+  setTimeout(() => {
+      // Changing the visibility of the 'loading' and 'started' elements to 'hidden'
+      loading.style.visibility = "hidden";
+      started.style.visibility = "hidden";
+  }, 250);
+}
+
+function onClosePlayer() {
+  // Reset the source of the player to stop any playing media
+  playerSource.src = "";
+
+  // Resume music and other background sounds, if any
+  music.play();
+
+  // Animate the closing of the player
+  gsap.to(player, {
+      opacity: 0,
+      duration: 0.5,
+      onComplete: () => {
+          player.style.visibility = "hidden";
+      }
+  });
+
+  // Set a timeout to ensure video look is disabled after the animations
+  setTimeout(() => {
+      videoLook = false;
+  }, 500);
+}
+// *********************************************************
+
 // Axis helper
 // const axesHelper = new THREE.AxesHelper(300);
 // snowScene.add(axesHelper);
+
+// Function switches between cameras when "Enter Museum" button is clicked
+function switchCamera(cameraToSelect) {
+  if (cameraToSelect === 'santaCamera') {
+    renderer.camera = santaCamera;
+    renderer.scene = santaScene;
+
+    // Clearing current html elements
+    const navBar = document.querySelector('.nav-bar');
+    const titleImage = document.querySelector('.title-image');
+    const vignetteDiv = document.querySelector('.vignette');
+  
+    navBar.style.display = 'none';
+    titleImage.style.display = 'none';
+    vignetteDiv.style.display = 'none';
+
+    animate();
+  } else {
+    renderer.camera = mainCamera;
+    renderer.scene = snowScene;
+  }
+}
 
 // Creating and calling animation loop
 function animate() {
@@ -209,8 +591,8 @@ function animate() {
   // Update snow particles
   updateSnowParticles();
 
-  // Render snow scene
-  renderer.render(snowScene, camera);
+  // Render scene and camera based on current user interaction
+  renderer.render(renderer.scene, renderer.camera);
   
   controls.update();
 }
@@ -223,10 +605,22 @@ window.addEventListener('resize', () => {
   sizes.width = window.innerWidth;
   sizes.height = window.innerHeight;
 
-  // Update camera
-  camera.aspect = sizes.width / sizes.height;
-  camera.updateProjectionMatrix();
+  // Update main camera
+  mainCamera.aspect = sizes.width / sizes.height;
+  mainCamera.updateProjectionMatrix();
+  
+  // Update santaCamera aspect ratio
+  santaCamera.aspect = sizes.width / sizes.height;
+  santaCamera.updateProjectionMatrix();
+  // Update background camera aspect ratio
+  backgroundCamera.left = -sizes.width / 2;
+  backgroundCamera.right = sizes.width / 2;
+  backgroundCamera.top = sizes.height / 2;
+  backgroundCamera.bottom = -sizes.height / 2;
+  backgroundCamera.updateProjectionMatrix();
+
   renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 3));
 });
 
 // Removing disply: none attribute on title image and nav bar
@@ -241,7 +635,6 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 // Timeline magic *****************************************************
-const tl = gsap.timeline({defaults: {duration: 1}});
 
 // Add opacity animations for all meshes using fromTo with staggered delays
 tl.fromTo(santaMesh.position, { y: 180 }, { y: 61, delay: 1 });
@@ -292,5 +685,9 @@ tl.eventCallback("onComplete", () => {
       meshElement.position.set(newX, newY, meshElement.position.z);
     });
   });
+
+  window.addEventListener("mousedown", (e) => {
+    switchCamera('santaCamera');
+  })
 });
 // ********************************************************************
